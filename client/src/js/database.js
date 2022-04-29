a809b09530ec8a325dc2de05750b955b32619c3c
aditore <anthonyditore@gmail.com>
2022-04-28 17:21:53 -0700

    put method indexdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT in the database');
+
+  //connection to databse plus version
+  const textDb = await openDB('jate', 1);
+  //new transaction to and specify privileges
+  const text = textDb.transaction('jate', 'readwrite');
+  //open the object store
+  const store = text.objectStore('jate');
+  //update database
+  const req = store.put({ text: content });
+  //confirm request
+  const res = await req;
+  console.log('Data saved to DB', res);
+  return res; 
+}
 
 // TODO: Add logic for a method that gets all the content from the database -complete
 export const getDb = async () => {
