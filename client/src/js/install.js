78931e15457f5abfbb5756a9ad346e8e44dba660
aditore <anthonyditore@gmail.com>
2022-04-29 09:39:09 -0700

    making install code cleaner

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,26 +1,35 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    console.log('beforeinstallprompt', event);
+    //stash the event
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    console.log('butInstall-clicked');
     const promptEvent = window.deferredPrompt;
-    
+
     if (!promptEvent) {
         return;
     }
 
     promptEvent.prompt();
+    //check the result in the log
+    const res = await promptEvent.userChoice;
+    console.log('userChoice', res);
+    //reset variable
     window.deferredPrompt = null;
     butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    console.log('appinstalled', event);
     window.deferredPrompt = null;
+    window.prompt('App Installed, check your desktop!');
 });
